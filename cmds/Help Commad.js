3c8685c5cfcb3336fa7be212d4000cee4f100fb5
ExpYT <36660507+ExpYT@users.noreply.github.com>
2018-02-21 20:13:20 +1000

    Add files via upload

---
+++ cmds/Help Commad.js
@@ -0,0 +1,11 @@
+var Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+	const embed = new Discord.RichEmbed();
+	.setTitle('XpBot Command Help');
+	.setColor('#4caf50');
+	.addField('Ping - Will return the ping for the bot, correct use: !ping')
+}
+ module.exports.help = {
+    name: "help"
+    }
