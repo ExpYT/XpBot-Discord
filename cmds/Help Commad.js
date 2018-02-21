746269ed8361358c650596beef19374bb39ccf8c
ExpYT <36660507+ExpYT@users.noreply.github.com>
2018-02-21 20:16:10 +1000

    Update Help Commad.js

--- cmds/Help Commad.js
+++ cmds/Help Commad.js
@@ -2,9 +2,12 @@ var Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 	const embed = new Discord.RichEmbed();
-	.setTitle('XpBot Command Help');
-	.setColor('#4caf50');
-	.addField('Ping - Will return the ping for the bot, correct use: !ping')
+	embed.setTitle('XpBot Command Help');
+	embed.setColor('#4caf50');
+	embed.addField('Ping - Will return the ping for the bot, correct use: !ping')
+	
+
+	message.channel.send(embed)
 }
  module.exports.help = {
     name: "help"
